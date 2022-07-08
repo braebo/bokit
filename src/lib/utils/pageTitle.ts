f90f1cf391c873a85fd25ffe2b7bc9104069aab9
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-07-08 13:48:35 -0400

    [feat] improved pageTitle

--- src/lib/utils/pageTitle.ts
+++ src/lib/utils/pageTitle.ts
@@ -5,8 +5,13 @@ import { get } from 'svelte/store'
  * Gets the current page title based on the route name.
  */
 export const pageTitle = (path: string = get(page).url.pathname) => {
+	// Use status code if there's an error
+	if (get(page).error) return get(page).status
+
 	if (path === '/') return 'Home'
+
 	const title = path.split('/')[1]
 	const capilized = title.charAt(0).toUpperCase() + title.slice(1)
+
 	return capilized || ''
 }
