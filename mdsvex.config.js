97d05be1b7533b65c6eed827331f50de58ea1954
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-07-11 14:10:20 -0400

    [config] remove remark-github

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,5 +1,4 @@
 import autolinkHeadings from 'rehype-autolink-headings'
-import github from 'remark-github'
 import slug from 'rehype-slug'
 import abbr from 'remark-abbr'
 
@@ -8,15 +7,7 @@ const mdsvexConfig = {
 	smartypants: {
 		dashes: 'oldschool'
 	},
-	remarkPlugins: [
-		[
-			github,
-			{
-				repository: 'https://github.com/fractalhq/frackit.git'
-			}
-		],
-		abbr
-	],
+	remarkPlugins: [abbr],
 	rehypePlugins: [
 		slug,
 		[
