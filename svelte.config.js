fb1ca744a161e92b4125d6913eca32b9972bbb24
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-10-26 12:58:45 -0400

    [deps] remove mdsvex blog stuff

--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,6 @@ import mdsvexConfig from './mdsvex/mdsvex.config.js'
 import vercel from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
 import { mdsvex } from 'mdsvex'
-import fg from 'fast-glob'
-
-// Get all of the blog post paths for prerendering.
-const blog = fg.sync(['./src/routes/blog/**/*.md'])
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,21 +12,14 @@ const config = {
 		}),
 		mdsvex(mdsvexConfig)
 	],
-	kit: {
-		adapter: vercel(),
-		prerender: {
-			entries: blog.map((entry) => entry.replace('./src/routes/blog/(post)', '/blog'))
-		}
-	},
+	kit: { adapter: vercel() },
 	vitePlugin: {
-		experimental: {
-			// https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/config.md#inspector
-			inspector: {
-				toggleButtonPos: 'bottom-left',
-				toggleKeyCombo: 'meta-shift',
-				showToggleButton: 'always',
-				holdMode: true
-			}
+		// https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/config.md#inspector
+		inspector: {
+			toggleButtonPos: 'bottom-left',
+			toggleKeyCombo: 'meta-shift',
+			showToggleButton: 'always',
+			holdMode: true
 		}
 	}
 }
