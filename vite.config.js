d6bf94b91d0e93855cb155880043e89b1bfcad1b
Fractal <55504972+FractalHQ@users.noreply.github.com>
2023-02-08 22:54:30 -0500

    [fix] remove vite-imageTools

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { imagetools } from 'vite-imagetools'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit(), imagetools()],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
